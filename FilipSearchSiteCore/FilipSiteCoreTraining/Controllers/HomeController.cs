64a4f410e878447293e4da2936bb5220efdaf63d
Mihalut Filip <filipmihalut@gmail.com>
2023-03-03 20:28:40 +0530

    Add files via upload

---
+++ FilipSearchSiteCore/FilipSiteCoreTraining/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FilipSiteCoreTraining.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DemoComponent()
+        {
+            var item = Sitecore.Context.Database.GetItem("{92E860DE-3097-4835-9B18-43579B3C5F9F}");
+            string str = item.Fields["Copyright"].Value;
+            return View("~/Views/ControllerDemo.cshtml", item);
+        }
+    }
+}
